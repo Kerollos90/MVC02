59f9ab08a020dc35e82c9cb70bd011d227eec8cb
Kerollos90 <emadkero318@gmail.com>
2024-09-20 09:33:05 +0300

    Part 02 AccountController (Login - Action & View)

--- MVC02/Controllers/AccountController.cs
+++ MVC02/Controllers/AccountController.cs
@@ -1,6 +1,7 @@
 ﻿using Company.Data.Entity;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Templates.Blazor;
 using MVC02.Models;
 
 namespace MVC02.Controllers
@@ -8,10 +9,12 @@ namespace MVC02.Controllers
     public class AccountController : Controller
     {
         private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
 
-        public AccountController(UserManager<ApplicationUser> userManager)
+        public AccountController(UserManager<ApplicationUser> userManager ,SignInManager<ApplicationUser> signInManager)
         {
             _userManager = userManager;
+            _signInManager = signInManager;
         }
         public IActionResult LogIn()
         {
@@ -19,33 +22,34 @@ namespace MVC02.Controllers
         }
 
         [HttpPost]
-        public async Task<IActionResult> LogIn(SignUpViewModel signUp)
+        public async Task<IActionResult> LogIn(LoginViewModel login)
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser
-                {
-                    UserName = signUp.Email.Split("@")[0],
-                    Email = signUp.Email,
-                    FirstName = signUp.FirstName,
-                    LastName = signUp.LastName,
-                    IsActive = true
+					var user = await _userManager.FindByEmailAsync(login.Email);
+
+				if (user is not null)
+				{
+					if (await _userManager.CheckPasswordAsync(user, login.Password))
+					{
+
+						var result = await _signInManager.PasswordSignInAsync(user, login.Password, login.RememberMe, true);
+
+						if (result.Succeeded)
+							return RedirectToAction("Index" ,"Home");
+					}
+
+					
+				}
+						ModelState.AddModelError("", "Incorrect Email Or Pasword");
+					return View(login);
 
 
-                };
 
-                var result = await _userManager.CreateAsync(user, signUp.Password);
-
-                    if(result.Succeeded)
-                    return RedirectToAction("SignIn");
-
-
-                foreach (var e in result.Errors)
-                    ModelState.AddModelError("",e.Description);
             
             }
 
-            return View(signUp);
+               return View(login);
         }
 
 
---
+++ MVC02/Models/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC02.Models
+{
+    public class LoginViewModel
+    {
+
+        [Required(ErrorMessage = "Email Is Required")]
+        [EmailAddress(ErrorMessage = "Invalid Format Email")]
+
+        public string Email { get; set; }
+        [Required(ErrorMessage = "password is required")]
+        public string Password { get; set; }
+
+        public bool RememberMe { get; set; }
+
+
+    }
+}
