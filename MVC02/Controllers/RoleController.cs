cde814dc272f75927bbe68aab24f2e6897f5f0b5
Kerollos90 <emadkero318@gmail.com>
2024-09-22 15:56:48 +0300

    Part 07 RolesController(AddOrRemoveUsers - Get) .

--- MVC02/Controllers/RoleController.cs
+++ MVC02/Controllers/RoleController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Company.Data.Entity;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using MVC02.Models;
@@ -9,12 +10,15 @@ namespace MVC02.Controllers
     public class RoleController : Controller
     {
         private readonly RoleManager<IdentityRole> _roleController;
-        private readonly ILogger<RoleController> _logger;
 
-        public RoleController(RoleManager<IdentityRole> roleController ,ILogger<RoleController> logger) 
+        private readonly ILogger<RoleController> _logger;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RoleController(RoleManager<IdentityRole> roleController ,ILogger<RoleController> logger , UserManager<ApplicationUser> userManager ) 
         {
             _roleController = roleController;
             _logger = logger;
+            _userManager = userManager;
         }
         public async  Task<IActionResult> Index()
         {
@@ -177,6 +181,54 @@ namespace MVC02.Controllers
 
 
         }
+
+
+
+
+
+
+        public async Task<IActionResult> AddOrRemoveUsers(string RoleId)
+        {
+            var role = await _roleController.FindByIdAsync(RoleId);
+            if(role is null)
+                return RedirectToAction(nameof(NotFound),"Home");
+
+            var users = await _userManager.Users.ToListAsync();
+
+            var UserInRoles = new List<UsersInRolesViewModel>();
+
+            foreach (var user in users)
+            {
+                var userInRole = new UsersInRolesViewModel
+                {
+                    Id = user.Id,
+                    Name = user.UserName
+
+
+                };
+
+                if (await _userManager.IsInRoleAsync(user, role.Name))
+                    userInRole.IsSelected = true;
+                else
+                    userInRole.IsSelected = false;
+
+                UserInRoles.Add(userInRole);
+
+
+            }
+            
+
+
+
+
+
+            return View(UserInRoles);
+        
+        }
+
+
+
+
     }
 }
 
---
+++ MVC02/Models/UsersInRolesViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC02.Models
+{
+    public class UsersInRolesViewModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public bool IsSelected { get; set; }
+
+
+
+
+    }
+}
