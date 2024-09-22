017268cc7860adcc0cd34d959aba81e74f77dd45
Kerollos90 <emadkero318@gmail.com>
2024-09-22 12:20:37 +0300

    Part 05 RolesController(Index & Create).

---
+++ MVC02/Controllers/RoleController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace MVC02.Controllers
+{
+    public class RoleController : Controller
+    {
+        private readonly RoleManager<IdentityRole> _roleController;
+        private readonly ILogger<RoleController> _logger;
+
+        public RoleController(RoleManager<IdentityRole> roleController ,ILogger<RoleController> logger) 
+        {
+            _roleController = roleController;
+            _logger = logger;
+        }
+        public async  Task<IActionResult> Index()
+        {
+            var role = await _roleController.Roles.ToListAsync();
+            return View(role);
+        }
+
+
+        public IActionResult Create()
+        { 
+            return View();
+        
+        
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(IdentityRole role)
+        {
+            if (ModelState.IsValid)
+            {
+                var result = await _roleController.CreateAsync(role);
+
+                if(result.Succeeded)
+                    return RedirectToAction("Index");
+
+                foreach (var item in result.Errors)
+                    _logger
+                        .LogError(item.Description);
+            }
+                    return View(role);
+        }
+    }
+}
+
