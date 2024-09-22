6f959dacc426f2ef9f9c6ef859d1dabf103938ed
Kerollos90 <emadkero318@gmail.com>
2024-09-22 06:26:07 +0300

    Part 01 UsersController(Index).

---
+++ MVC02/Controllers/UserController.cs
@@ -0,0 +1,37 @@
+﻿using Company.Data.Entity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Drawing.Text;
+
+namespace MVC02.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserController(UserManager<ApplicationUser> userManager) 
+        {
+           
+            _userManager = userManager;
+        }
+
+        public async Task<IActionResult> Index(string SearchInp )
+        {
+            List<ApplicationUser> users;
+
+            if (string.IsNullOrEmpty(SearchInp))
+                users = await _userManager.Users.ToListAsync();
+
+                else
+                users = await _userManager.Users
+                              .Where(x => x.NormalizedEmail.Trim().Contains(SearchInp.Trim().ToUpper()))
+                              .ToListAsync();
+
+           
+
+
+            return View(users);
+        }
+    }
+}
